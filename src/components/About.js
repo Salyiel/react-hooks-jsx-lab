31d45e9d01149cc97cd44d12489336113eaf6b88
Salyiel <162126755+Salyiel@users.noreply.github.com>
2024-05-16 16:51:29 +0300

    Complete lab assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>
+  return (
+  <div id="about">
     <h2> About Me</h2>
     <p>I am currently learning to use React through playing with this website project</p>
       <img src={image} alt="I made this"></img>
-  </div>;
+  </div>
+  )
 }
 
 export default About;
